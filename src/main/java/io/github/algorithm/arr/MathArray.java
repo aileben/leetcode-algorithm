df918b87d0929897c17a429536f8178e43834a9d
冯宇鹏 <fengyupeng@bonc.com.cn>
2021-02-01 10:17:08 +0800

    1/4 二进制求和 给你两个二进制字符串，返回它们的和（用二进制表示）。 输入为 非空 字符串且只包含数字 1 和 0。

--- src/main/java/io/github/algorithm/arr/MathArray.java
+++ src/main/java/io/github/algorithm/arr/MathArray.java
@@ -206,7 +206,52 @@ public class MathArray {
     return flag;
   }
 
+  /*
+  这题看着很麻烦，实际上也有些麻烦，但只要把注意的点都搞清楚，其实代码结构是很简单的
+
+  先设定numSeen，dotSeen和eSeen三种boolean变量，分别代表是否出现数字、点和E
+  然后遍历目标字符串
+  1.判断是否属于数字的0~9区间
+  2.遇到点的时候，判断前面是否有点或者E，都需要return false
+  3.遇到E的时候，判断前面数字是否合理，是否有E，并把numSeen置为false，防止E后无数字
+  4.遇到-+的时候，判断是否是第一个，如果不是第一个判断是否在E后面，都不满足则return false
+  5.其他情况都为false
+  最后返回numSeen的结果即可
+  */
+
+  public static boolean isNumber1(String s) {
+    if (s == null || s.length() == 0) return false;
+    boolean numSeen = false;
+    boolean dotSeen = false;
+    boolean eSeen = false;
+
+    char arr[] = s.trim().toCharArray();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] >= '0' && arr[i] <= '9') {
+        numSeen = true;
+      } else if (arr[i] == '.') {
+        if (dotSeen || eSeen) {
+          return false;
+        }
+        dotSeen = true;
+      } else if (arr[i] == 'E' || arr[i] == 'e') {
+        if (eSeen || !numSeen) {
+          return false;
+        }
+        eSeen = true;
+        numSeen = false;
+      } else if (arr[i] == '+' || arr[i] == '-') {
+        if (i != 0 && arr[i - 1] != 'e' && arr[i - 1] != 'E') {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+    return numSeen;
+  }
+
   public static void main(String[] args) {
-    System.out.println(isNumber(" 0.1 "));
+    System.out.println(isNumber1(" 0.1"));
   }
 }
---
+++ src/main/java/io/github/algorithm/link/AddBinaryDemo.java
@@ -0,0 +1,88 @@
+package io.github.algorithm.link;
+
+public class AddBinaryDemo {
+
+  /*
+  1/4 二进制求和
+  给你两个二进制字符串，返回它们的和（用二进制表示）。
+  输入为 非空 字符串且只包含数字 1 和 0。
+    1、获取两个字符串的长度
+    2、声明进位临时变量，和临时整型数组（临时数组的长度是较长数组加一）
+    3、将字符串转为字符串数组，逆序遍历，低位二进制求和进位放到临时变量中
+    4、将整型数据转为字符串
+    5、默认字符串最长的长度不超过整型
+    */
+  public static String addBinary(String a, String b) {
+    int aLen = a.length(), bLen = b.length();
+
+    int maxLen = aLen >= bLen ? aLen : bLen;
+    int divLeb = aLen >= bLen ? aLen - bLen : bLen - aLen;
+
+    char[] aArr = a.toCharArray();
+    char[] bArr = b.toCharArray();
+
+    int[] aTemp = new int[maxLen];
+    int[] bTemp = new int[maxLen];
+
+    for (int i = 0; i < maxLen; i++) {
+      if (aLen >= bLen) {
+        aTemp[i] = aArr[i] - '0';
+        if (i <= maxLen && i >= divLeb) {
+          bTemp[i] = bArr[i - divLeb] - '0';
+        } else {
+          bTemp[i] = 0;
+        }
+      } else {
+        bTemp[i] = bArr[i] - '0';
+        if (i <= maxLen && i >= divLeb) {
+          aTemp[i] = aArr[i - divLeb] - '0';
+        } else {
+          aTemp[i] = 0;
+        }
+      }
+    }
+
+    int[] arr = new int[maxLen];
+    int temp = 0;
+    for (int i = arr.length - 1; i >= 0; i--) {
+      arr[i] = (aTemp[i] + bTemp[i] + temp) % 2;
+      temp = (aTemp[i] + bTemp[i] + temp) / 2;
+    }
+    StringBuilder result = new StringBuilder(temp == 0 ? "" : "1");
+    for (int i = 0; i < maxLen; i++) {
+      result.append(arr[i]);
+    }
+    return result.toString();
+  }
+
+  public static void main(String[] args) {
+    //    String a = "11000000111";
+    //    String b = "10101100111001000100010101111101010010000100101";
+
+    String a = "1101";
+    String b = "1011";
+
+    System.out.println(addBinary1(a, b));
+  }
+
+  public static String addBinary1(String a, String b) {
+    StringBuffer ans = new StringBuffer();
+    // 二进制相加，末尾对齐，逐位相加，逢二进一
+    int n = Math.max(a.length(), b.length()), carry = 0; // carry表示上一位置的进位，初始为0
+    // 从低位开始遍历相加，a、b中短的位置补0
+    for (int i = 0; i < n; ++i) {
+      carry += i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
+      carry += i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0;
+      ans.append((char) (carry % 2 + '0'));
+      carry /= 2;
+    }
+    // 进位有值，存入答案字符串中
+    if (carry > 0) {
+      ans.append('1');
+    }
+    // 最终将答案串反转
+    ans.reverse();
+
+    return ans.toString();
+  }
+}
